26407c0806b1eb4f9b6a04daba345386c7b363db
BanGO <66056469+fffdee@users.noreply.github.com>
2024-05-22 09:07:15 +0800

    实现串口和初步下载

--- loadfile.py
+++ loadfile.py
@@ -1,14 +1,107 @@
-import sys
+import sys,os
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QFileDialog,QHBoxLayout,QWidget,QVBoxLayout,QComboBox,QLabel,QTextEdit,QProgressBar
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtGui import QPixmap,QIcon
 from PyQt5.QtCore import Qt,QThread,pyqtSignal
 import serial
+import struct
 
+class BinFileReader(QThread):
+    data_signal = pyqtSignal(bytearray)  # 定义一个信号，用于传输读取到的数据
 
+    def __init__(self, file_path,serial_port, baud_rate,parent=None):
+        super().__init__(parent)
+        self.file_path = file_path
+        self.data = bytearray()
+        self.serial_port = serial_port
+        self.baud_rate = baud_rate
+    def run(self):
+        # 打开文件并读取内容
+        with open(self.file_path, 'rb') as file:
+            self.data = file.read()
+        self.data = bytearray(self.data) 
+        self.data_signal.emit(self.data)  # 发射信号，传递读取到的数据
+        try:
+            self.ser = serial.Serial(self.serial_port, self.baud_rate)
+            if self.ser.is_open:
+             print("串口已成功打开。")
+            else:
+             print("串口打开失败。")
+            
+        except serial.SerialException as e:
+            print(f"串口打开时发生错误: {e}")
+            
+        
+        self.ser.flushInput()
+        file_name = self.file_path.split('/')[-1]
+        # 文件大小
+        file_size = os.path.getsize(self.file_path)
+        # 初始化状态
+        state = 'SEND_FILE'
+        # 初始化文件索引
+        file_index = 0
+        # 初始化文件数据
+        file_data = bytearray()
+        # 读取文件内容
+        with open(self.file_path, 'rb') as file:
+            file_data = file.read()
+        # 发送文件信息
+        self.ser.write(struct.pack('B', 0x01))  # 文件信息长度为1
+        self.ser.write(struct.pack('B', len(file_name)))  # 文件名长度
+        self.ser.write(file_name.encode())  # 文件名
+        self.ser.write(struct.pack('>I', file_size))  # 文件大小
+        self.ser.write(struct.pack('>I', 0))  # 文件校验和
+        # 发送文件内容
+        for i in range(0, file_size, 128):
+            if i + 128 <= file_size:
+                self.ser.write(struct.pack('B', 0x02))  # 数据包类型
+                self.ser.write(struct.pack('>I', i))  # 数据包起始位置
+                self.ser.write(file_data[i:i+128])  # 数据包内容
+                self.ser.write(struct.pack('>I', 0))  # 数据包校验和
+                
+            else:
+                self.ser.write(struct.pack('B', 0x03))  # 数据包类型
+                self.ser.write(struct.pack('>I', i))  # 数据包起始位置
+                self.ser.write(file_data[i:])  # 数据包内容
+                self.ser.write(struct.pack('>I', 0))  # 数据包校验和
+        self.data_signal.emit(bytearray("sendok",'utf-8'))
+
+    # def ymodem_send(ser, file_path):
+    #     # 文件路径
+    #     file_name = file_path.split('/')[-1]
+    #     # 文件大小
+    #     file_size = os.path.getsize(file_path)
+    #     # 初始化状态
+    #     state = 'SEND_FILE'
+    #     # 初始化文件索引
+    #     file_index = 0
+    #     # 初始化文件数据
+    #     file_data = bytearray()
+    #     # 读取文件内容
+    #     with open(file_path, 'rb') as file:
+    #         file_data = file.read()
+    #     # 发送文件信息
+    #     ser.write(struct.pack('B', 0x01))  # 文件信息长度为1
+    #     ser.write(struct.pack('B', len(file_name)))  # 文件名长度
+    #     ser.write(file_name.encode())  # 文件名
+    #     ser.write(struct.pack('>I', file_size))  # 文件大小
+    #     ser.write(struct.pack('>I', 0))  # 文件校验和
+    #     # 发送文件内容
+    #     for i in range(0, file_size, 128):
+    #         if i + 128 <= file_size:
+    #             ser.write(struct.pack('B', 0x02))  # 数据包类型
+    #             ser.write(struct.pack('>I', i))  # 数据包起始位置
+    #             ser.write(file_data[i:i+128])  # 数据包内容
+    #             ser.write(struct.pack('>I', 0))  # 数据包校验和
+    #         else:
+    #             ser.write(struct.pack('B', 0x03))  # 数据包类型
+    #             ser.write(struct.pack('>I', i))  # 数据包起始位置
+    #             ser.write(file_data[i:])  # 数据包内容
+    #             ser.write(struct.pack('>I', 0))  # 数据包校验和
 
 class SerialThread(QThread):
     new_data_signal = pyqtSignal(str)  # 定义一个信号，用于传输接收到的数据
+    finished_signal = pyqtSignal(serial.Serial)
     def __init__(self, serial_port, baud_rate):
         super().__init__()
         self.serial_port = serial_port
@@ -21,9 +114,10 @@ class SerialThread(QThread):
              print("串口已成功打开。")
             else:
              print("串口打开失败。")
-            return
+            
         except serial.SerialException as e:
             print(f"串口打开时发生错误: {e}")
+            
         
         self.ser.flushInput()
        # self.ser.write(B"helloWorld!\n")
@@ -35,8 +129,9 @@ class SerialThread(QThread):
                 print(f"Received data: {data}")
                 self.new_data_signal.emit(data)  # 发射信号，传递接收到的数据
             # data = self.ser.readline().decode('utf-8').strip()
-            
-           # print("n")
+    def close_serial(self):
+        if self.ser is not None:
+            self.ser.close()
 
 
 class MainWindow(QMainWindow):
@@ -74,15 +169,7 @@ class MainWindow(QMainWindow):
         self.COMText.setStyleSheet("QLabel { color: #000; font-size: 20px; }")
         self.COMText.setText("选择串口:")
         
-        self.openCOM = QPushButton("打开串口", self)
-        self.openCOM.setCheckable(True)
-        self.openCOM.setChecked(False)
-        self.openCOM.setFixedWidth(100)
-        self.openCOM.setFixedHeight(40)
-        self.openCOM.setStyleSheet(("QPushButton { background-color: #111111; color: white; }"
-                     "QPushButton:hover { background-color: #FFFFFF;color: black; }"
-                     "QPushButton:pressed { background-color: #00FF00; }"))
-        self.openCOM.toggled.connect(self.switch_changed)
+    
         
          # 创建一个可用串口选择器
         self.serial_port_combobox = QComboBox(self)
@@ -91,10 +178,10 @@ class MainWindow(QMainWindow):
         self.serial_port_combobox.setStyleSheet("QComboBox { background-color: #EEEEEE; color: #000; font-size: 20px; }")
         self.serial_port_combobox.currentIndexChanged.connect(self.serial_port_changed)
 
-        self.baudRateLabel = QLabel('波特率:')
+        self.baudRateLabel = QLabel('下载模式:')
         self.baudRateLabel.setStyleSheet("QLabel { color: #000; font-size: 20px; }")
         self.baudRateComboBox = QComboBox()
-        self.baudRateComboBox.addItems(['312500','9600', '19200', '38400', '57600', '115200' ])
+        self.baudRateComboBox.addItems(['MIDI','9600', '19200', '38400', '57600', '115200' ])
        
         self.baudRateComboBox.setFixedWidth(100)
         self.baudRateComboBox.setFixedHeight(40)
@@ -110,7 +197,17 @@ class MainWindow(QMainWindow):
         self.debugLabel.setStyleSheet("QLabel { color: #000; font-size: 20px; }")
         self.debugText = QTextEdit(self)
         self.debugText.setStyleSheet("QTextEdit { color: #000; font-size: 14px; }")
-
+    
+        self.debugbtn = QPushButton("调试模式", self)
+        self.debugbtn.setCheckable(True)
+        self.debugbtn.setChecked(False)
+        self.debugbtn.setFixedWidth(100)
+        self.debugbtn.setFixedHeight(40)
+        self.debugbtn.setStyleSheet(("QPushButton { background-color: #111111; color: white; }"
+                     "QPushButton:hover { background-color: #FFFFFF;color: black; }"
+                     "QPushButton:pressed { background-color: #00FF00; }"))
+        self.debugbtn.toggled.connect(self.debug_changed)
+        
         self.progressBar = QProgressBar(self)
         self.progressBar.setMinimum(0)
         self.progressBar.setMaximum(100)
@@ -146,7 +243,7 @@ class MainWindow(QMainWindow):
         self.layout2 = QHBoxLayout()
         self.layout2.setSpacing(20)
         self.layout2.addWidget(self.COMText)
-        self.layout2.addWidget(self.openCOM)
+       
         self.layout2.addWidget(self.serial_port_combobox)
         self.layout2.setContentsMargins(20, 0, 20, 0)
 
@@ -159,17 +256,23 @@ class MainWindow(QMainWindow):
         self.layout3.addWidget(self.stopBitsLabel)
         self.layout3.setContentsMargins(20, 0, 20, 0)
 
-        self.layout4 = QVBoxLayout()
+        self.layout4 = QHBoxLayout()
         self.layout4.setSpacing(20)
         self.layout4.addWidget(self.debugLabel)
-        self.layout4.addWidget(self.debugText) 
+        self.layout4.addWidget(self.debugbtn) 
         self.layout4.setContentsMargins(20, 0, 20, 0)
 
         self.layout5 = QHBoxLayout()
         self.layout5.setSpacing(10)
-        self.layout5.addWidget(self.progressBar)
-        self.layout5.addWidget(self.DOWNLOAD)
+        self.layout5.addWidget(self.debugText)
+        
         self.layout5.setContentsMargins(20, 0, 20, 0)
+
+        self.layout6 = QHBoxLayout()
+        self.layout6.setSpacing(10)
+        self.layout6.addWidget(self.progressBar)
+        self.layout6.addWidget(self.DOWNLOAD)
+        self.layout6.setContentsMargins(20, 0, 20, 0)
 
         # 总布局
         self.all.addLayout(self.layout0)
@@ -178,8 +281,8 @@ class MainWindow(QMainWindow):
         self.all.addLayout(self.layout3)
         self.all.addLayout(self.layout4)
         self.all.addLayout(self.layout5)
-       
-        
+        self.all.addLayout(self.layout6)
+
         # 设置中心窗口
         central_widget = QWidget()
         central_widget.setLayout(self.all)
@@ -199,7 +302,8 @@ class MainWindow(QMainWindow):
         self.serial_ports = QSerialPortInfo.availablePorts()
         # 将串口名称添加到下拉列表中
         for port in self.serial_ports:
-            self.serial_port_combobox.addItem(port.portName())
+            if port.portName()!= 'COM1':
+                self.serial_port_combobox.addItem(port.portName())
  
     def serial_port_changed(self, index):
         # 当串口选择变化时，更新文本编辑框
@@ -211,66 +315,114 @@ class MainWindow(QMainWindow):
     def select_file(self):
         # 显示文件选择对话框
         options = QFileDialog.Options()
-        file_path, _ = QFileDialog.getOpenFileName(self, "选取文件", "", "下载文件 (*.bin)", options=options)
+        self.file_path, _ = QFileDialog.getOpenFileName(self, "选取文件", "", "下载文件 (*.bin)", options=options)
         
         # 如果用户选择了文件
-        if file_path:
+        if  self.file_path:
             # 更新文本编辑框的内容
-            self.file_path_edit.setText(file_path)
-            self.debugText.append("读取的文件："+file_path)
+            self.file_path_edit.setText( self.file_path)
+            self.debugText.append("读取的文件："+ self.file_path)
 
-    def switch_changed(self, state):
+    def debug_changed(self, state):
         if state == True:
             
-            self.startThread()
+            
            # self.debugText.append('打开串口：'+str(self.selected_port)+','+str(self.baud_rate))
-            self.openCOM.setText("关闭串口")
+            self.baudRateLabel.setText("波特率:")
+            self.baudRateComboBox.setCurrentIndex(5)
+            self.startThread()
+            self.debugText.append('打开调试')
+            self.debugbtn.setText("关闭调试")
         else:
-            self.debugText.append('关闭串口')
-            self.stopThread()
-            self.openCOM.setText("打开串口")
-         
+            
+            self.restartApp()
+            self.debugText.append('关闭调试')
+            self.debugbtn.setText("调试模式")
+            self.baudRateLabel.setText("下载模式:")
+   
     def Download_changed(self, state):
         if state == True:
             self.debugText.append("开始下载")
+            self.selected_port = self.serial_port_combobox.currentText()
+            if self.baudRateComboBox.currentText()=="MIDI":
+                self.baud_rate = 312500
+            else :
+                self.baud_rate = int(self.baudRateComboBox.currentText())
+            self.debugText.append('打开串口：'+str(self.selected_port)+','+str(self.baud_rate))
+            self.bin_reader = BinFileReader(self.file_path,self.selected_port,self.baud_rate)
+            self.bin_reader.data_signal.connect(self.displayData)  # 连接信号和槽函数
+            self.bin_reader.start()
             self.DOWNLOAD.setText("取消下载")
         else:
+            self.restartApp()
             self.debugText.append('取消下载')
             self.DOWNLOAD.setText("开始下载")
     #  ————————————————————————————————————————————————串口接收线程系列函数——————————————————————————————————————————————————            
     def startThread(self):
         # 获取新的串口和波特率设置
         self.selected_port = self.serial_port_combobox.currentText()
-        self.baud_rate = int(self.baudRateComboBox.currentText())
+        if self.baudRateComboBox.currentText()=="MIDI":
+            self.baud_rate = 312500
+        else :
+            self.baud_rate = int(self.baudRateComboBox.currentText())
         self.serial_thread = SerialThread(self.selected_port,self.baud_rate)
         self.serial_thread.new_data_signal.connect(self.appendText)  # 连接信号和槽函数
+        self.serial_thread.finished_signal.connect(self.closeSerial)  # 连接信号和槽函数
         self.debugText.append('打开串口：'+str(self.selected_port)+','+str(self.baud_rate))
+        
         # 停止当前的线程（如果正在运行）
         if self.serial_thread.isRunning():
             self.stopThread()
-            print("有在运行")
+            
         # 启动新的线程
         self.serial_thread.start()
 
-       
+    def closeSerial(self, ser):
+        # 假设 ser 是从线程中传递的串口句柄
+        if ser is not None:
+            ser.close()   
 
     def stopThread(self):
-       print("结束")
-       try:
-           self.serial_thread.ser.close()
-       except serial.SerialException as e:
-            print(f"串口打开时发生错误: {e}")
-       
-       self.serial_thread.quit()  # 请求线程退出
-       try:
-            self.serial_thread.wait()  # 等待线程退出
-            print("结束")
-       except Exception as e:
-            print(f"等待线程退出时出现错误: {e}")
-
+      self.serial_thread.close_serial()
+      self.serial_thread.quit()  # 请求线程退出
+      try:
+        self.serial_thread.wait()  # 等待线程退出
+        print("结束")
+      except Exception as e:
+        print(f"等待线程退出时出现错误: {e}")
+      if self.serial_thread.ser is not None:
+        try:
+            self.serial_thread.ser.close() 
+        except serial.SerialException as e:
+                print(f"串口打开时发生错误: {e}")
+        
+      
+        
+    
     def appendText(self, text):
         self.debugText.append(text)  # 将接收到的数据追加到文本编辑框
+
+    def displayData(self, data):
+        # 显示文件内容
+        bytes_data = bytes(data)
+
+        # 将bytes对象转换为十六进制字符串
+        hex_string = ' '.join(f'{b:02X}' for b in bytes_data)
+        self.debugText.append(hex_string )  
     # ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+    def restartApp(self):
+        # 关闭所有窗口
+        self.close()
+
+        # 创建一个新的QApplication实例
+        new_app = QApplication(sys.argv)
+
+        # 重新创建主窗口
+        new_main_window = MainWindow()
+        new_main_window.show()
+
+        # 退出旧的应用程序实例
+        sys.exit(new_app.exec_())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
